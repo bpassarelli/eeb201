a05483445502ff4ea743a74972b9d38e63df912d
bpassarelli <bpassarelli@saddleback.edu>
2015-09-16 16:12:38 -0700

    Add mini exercise 3.2.1

---
+++ Discrete_logistic_model_script.R
@@ -0,0 +1,33 @@
+# Bruno Passarelli
+# 16 September 2015
+# this is a script to simulate and plot the discrete logisitc model
+
+# set initial conditions and parameter values
+N0 <- 100
+RR <- 1.05
+ttMax <- 10
+
+# initialize variable to a vector of NA values
+# note that I adjusted the values because I already set the first value of the vector
+NN <- rep(NA, 11)
+NN[1]<-N0
+for(tt in 2:(ttMax+1)){
+  NN[tt] <- RR*NN[tt-1]
+}
+print(NN)
+  
+# Mini exercise 3.2.1
+N0 <- 100
+RR <- 0.95
+ttMax <- 15
+
+NN <- rep(NA, 16)
+NN[1]<-N0
+for(tt in 2:(ttMax+1)){
+  NN[tt] <- RR*NN[tt-1]
+}
+print(NN)
+years <- c(0:15)
+length(years)
+
+plot(years,NN)
