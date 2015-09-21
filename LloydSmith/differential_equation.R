0647d8e8d3f16f7d76c44ea59dd8087b4b36ec43
bpassarelli <bpassarelli@saddleback.edu>
2015-09-20 21:52:41 -0700

    add 4.1.1 mini-exercise

---
+++ LloydSmith/differential_equation.R
@@ -0,0 +1,63 @@
+# Bruno Passarelli
+# 20 September 2015
+# this is script to write a differential equation model in R
+
+rm(list=ls())
+library(deSolve)
+
+# 4.1 A first example of an ODE model: exponential growth
+expGrowthODE <- function(tt, yy, pars){ 
+  derivs <- pars["rr"] * yy
+  return(list(derivs))
+}
+
+# arguments must always be the same, in this specific order
+## tt is the time
+## yy is the state variable
+## pars is a vector of parameters
+## the names don't matter, as long as they are consistently used
+
+# generic syntax for the model
+# output <- lsoda(init, tseq, ODEfunction, pars)
+# init is the initial value of the state variable
+# tseq is a vector of the time points where the model will be evaluated
+# ODEfunction is a placeholder for the name of the function holding the model equations
+# pars is a vector containing any parameters used in the model
+
+# let's load some appropriate values into these variables
+init <- 1
+tseq <- seq(0, 20, by = 0.01)
+pars <- c(rr = 0.1)
+
+# let's run the model
+expGrowthOutput <- lsoda(init, tseq, expGrowthODE, pars)
+
+head(expGrowthOutput)
+
+par(mfrow = c(1,1))
+
+plot(expGrowthOutput[,1], expGrowthOutput[,2], col = "blue", type = "l")
+
+# 4.2 Mini-exercise
+# make a script that brings together the commands to make and run this model
+# setup statements
+library(deSolve)
+
+# setup function
+expGrowthODE <- function(tt, yy, pars){ 
+  derivs <- pars["rr"] * yy
+  return(list(derivs))
+}
+
+# set parameter values and initial conditions
+init <- 1
+tseq <- seq(0, 20, by = 0.01)
+pars <- c(rr = 0.1)
+
+# set calculations
+expGrowthOutput <- lsoda(init, tseq, expGrowthODE, pars)
+
+# display the results
+plot(expGrowthOutput[,1], expGrowthOutput[,2], col = "blue", type = "l")
+
+
