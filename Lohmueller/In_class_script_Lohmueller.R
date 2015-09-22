b226bd813c79f35f99659aa81d10a7ec0704c0b8
bpassarelli <bpassarelli@saddleback.edu>
2015-09-21 17:49:41 -0700

    add histograms and density plots

--- Lohmueller/In_class_script_Lohmueller.R
+++ Lohmueller/In_class_script_Lohmueller.R
@@ -2,15 +2,75 @@
 # 17 September 2015
 # this script is for Lohmueller's in class exercises
 
+rm(list=ls())
+
+setwd("/Users/bpassarelli/Documents/Bruno/UCLA/Fall 2015/EEB201/eeb201/Lohmueller")
+getwd()
+
+library(ape)
+library(geiger)
+library(ggplot2)
+
 # Histogram with two data sets in one plot
 s1<-rnorm(1000,mean=0,sd=1)
-s3<-rnorm(1000,mean=0,sd=1)
+s3<-rnorm(1000,mean=0,sd=3)
 
 # check the data
 head(s1)
 head(s3)
 
+# plot histograms of both in the same panel and save it to a file
 # create the pdf file
+pdf(file="Normal_hist.pdf", width=4, height=7);
+
+# open the file
+
+par(mfrow= c(2,1), mar=c(4, 4, 3, 2)) # this sets the plotting area and margins
+
+hist(s1,col=2,xlab="Value",main="Sigma1") # makes first histogram
+
+hist(s3,col=4,xlab="Value",main="Sigma3") # makes second histogram
+
+dev.off() # shuts off current output device
+quartz
+    2
+
+# let's get a little fancier
+# plot histograms of both on same panel and save to a file
+pdf(file="Normal_hist_fancy.pdf",width=4,height=7);
+
+# open the file
+
+par(mfrow=c(2,1), mar=c(4, 4, 3, 2)) # sets plotting area and margins
+
+hist(s1,col=2,xlab="Value",main=expression(paste(sigma,"=1")))
+# the above was used to make the first hist
+
+hist(s3,col=4,xlab="Value",main=expression(paste(sigma,"=3")))
+# the above was used to make the second hist
+
+dev.off()
+pdf
+  2
+
+# make a smooth density plot
+pdf(file="Normal_density.pdf", width=6,height=6) # this opens the file
+
+par(mfrow=c(1,1), mar=c(4, 4, 3, 2)) # set plotting area and margins
+
+plot(density(s1),col=2,lwd=4,xlab="Value",xlim=c(-15,15),main="Normal distribution")
+
+lines(density(s3),col=4,lwd=4) # add the SD=3 values
+
+legend(-15,0.4,c("sigma=1","sigma=3"),lwd=4,col=c(2,4),cex=1.5)
+# put a legend on
+# we can highlight the upper 10% of each distribution with a vertical line:
+abline(v=quantile(s1,0.9),lty=2,lwd=3,col=2) #puts a vertical line onto the plot for s1
+abline(v=quantile(s3,0.9),lty=2,lwd=3,col=4) #puts a vertical line onto the plot for s3
+dev.off()
+quartz
+     2
+
 pdf(file="normal_barplot.pdf", width=6, height=6)
 
 # determine the breaks
